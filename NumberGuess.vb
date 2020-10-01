834f999a2945ab5c276d26d1df5810b372271bb0
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-10-01 09:12:31 -0600

    add wip remove incorrect number

--- NumberGuess.vb
+++ NumberGuess.vb
@@ -16,6 +16,8 @@ Module NumberGuess
         Dim numberOfGuesses As Integer
         Dim correct As Boolean
         Dim userMessage As String
+        'Dim newNumber As Boolean
+        'Dim numberArray As Array
 
         lowNumber = 1
         highNumber = 100
@@ -24,27 +26,31 @@ Module NumberGuess
 
         Do While correct = False
             correct = False
+
+            'Do While newNumber = False
+            'newNumber = False
             guessedNumber = Int((Rnd() * highNumber) + lowNumber)
 
-            Console.WriteLine("Is your number " & guessedNumber & "?")
-            Console.WriteLine("1. yes")
-            Console.WriteLine("2. Too low")
-            Console.WriteLine("3. Too high")
+                ' Loop
+                Console.WriteLine("Is your number " & guessedNumber & "?")
+                Console.WriteLine("1. yes")
+                Console.WriteLine("2. Too low")
+                Console.WriteLine("3. Too high")
 
-            userMessage = Console.ReadLine()
+                userMessage = Console.ReadLine()
 
-            If userMessage = "Yes" Or userMessage = "1" Or userMessage = "1. Yes" Then
-                correct = True
-                Console.WriteLine("Got it in " & numberOfGuesses & "tries")
-            ElseIf userMessage = "Too low" Or userMessage = "2" Or userMessage = "2. Too low" Or userMessage = "low" Then
+                If userMessage = "Yes" Or userMessage = "1" Or userMessage = "1. Yes" Then
+                    correct = True
+                    Console.WriteLine("Got it in " & numberOfGuesses & "tries")
+                ElseIf userMessage = "Too low" Or userMessage = "2" Or userMessage = "2. Too low" Or userMessage = "low" Then
                 lowNumber = lowNumber + 1
                 highNumber = highNumber - 1
             ElseIf userMessage = "Too high" Or userMessage = "3" Or userMessage = "3. Too high" Or userMessage = "high" Then
-                highNumber = highNumber - 1
-            Else
-                Console.WriteLine("Please enter one of the options, please.")
-            End If
-        Loop
+                    highNumber = highNumber - 1
+                Else
+                    Console.WriteLine("Please enter one of the options, please.")
+                End If
+            Loop
     End Sub
 
 End Module
